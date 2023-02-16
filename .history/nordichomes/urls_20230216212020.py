fb1d351d719c96e57679916c3b1b4e36f5c5bc7b
Calin224 <sanducalinm@gmail.com>
2023-02-16 21:37:07 +0200

    part-12

---
+++ .history/nordichomes/urls_20230216212020.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from cart.views import add_to_cart,cart, checkout
+
+urlpatterns = [
+    path('cart/', cart, name='cart'),
+    path('cart/checkout', checkout, name='checkout'),
+    path('add_to_cart/<int:product_id>', add_to_cart, name='add_to_cart'),
+    path('admin/', admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- .history/nordichomes/urls_20230216212057.py
+++ .history/nordichomes/urls_20230216212057.py
@@ -3,10 +3,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
+from cart.views import add_to_cart,cart, checkout
 
 urlpatterns = [
     path('', include('core.urls')),
-    path('cart/', include('cart.urls')),
+    path('cart/', cart, name='cart'),
+    path('cart/checkout', checkout, name='checkout'),
+    path('add_to_cart/<int:product_id>', add_to_cart, name='add_to_cart'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
