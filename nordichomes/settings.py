dbf77e6080c97baf13a720ae2b60407e03a1c393
calin <sanducalinm@gmail.com>
2023-03-02 14:36:51 +0200

    debug_true

--- nordichomes/settings.py
+++ nordichomes/settings.py
@@ -43,8 +43,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u$v=&ha9@1w0tu--lcl9h##irwiz)zipgsg41hj!*qpek3lxe('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
